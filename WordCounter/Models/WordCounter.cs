ba4457c83959b544175dc097c2ce2c9149f7a732
Manasa Vesala <vesalamanasa@gmail.com>
2018-11-16 09:30:55 -0800

    Added main method and logic in WordeCounter.cs file

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -6,9 +6,28 @@ namespace WordCounter.Models
 {
   public static class WordCounter
   {
-    public static void GiveWordCounter(string word, string sentence)
+    public static int RepeatCounter(string word, string sentence)
     {
-      
-    } 
+      int count = 0;
+      string[] sentenceWords = sentence.Split();
+      for(int i = 0; i < sentenceWords.Length; i++)
+      {
+        if(word == sentenceWords[i])
+        {
+          count = count + 1;
+        }
+      }
+      return count;
+    }
+    
+    public static void Main()
+    {
+      Console.WriteLine("Please enter a Word that you want to count in a sentence:");
+      string userInputword = Console.ReadLine();
+      Console.WriteLine("Please enter a sentence:");
+      string userInputSentence = Console.ReadLine();
+      Console.WriteLine("The word "+ "'"+userInputword +"'"+" appered "+ RepeatCounter(userInputword.ToUpper(),userInputSentence.ToUpper())+" times.");
+    }
+
   }
 }
