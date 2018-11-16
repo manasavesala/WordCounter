3039e0c9c63890d14575040bf811d049009124c5
Manasa Vesala <vesalamanasa@gmail.com>
2018-11-16 08:30:16 -0800

    Added wordCounter test folder and Tests folder and .cd file.

---
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounter.Models;
+
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class WordCounterTest
+  {
+    
+
+  }
+
+}
---
+++ WordCounter/Models/WordCounter.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace WordCounter.Models
+{
+  public static class WordCounter
+  {
+    public static void GiveWordCounter(string word, string sentence)
+    {
+      
+    } 
+  }
+}
